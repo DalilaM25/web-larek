0eb02d42ead9ec4de1e3cd8985e40b7d1af1e8f4
DalilaM25 <melissa25.dm@gmail.com>
2024-05-29 21:01:55 +0300

    Первичная архитектура на проверку

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,31 +1,61 @@
-export interface ICard {
-    id: string,
-    description: string,
-    image: string,
-    title: string,
-    category: string,
-    price: number|null
-}
+export type Category =  'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
 
+//Интерфейс карточки товара
+export interface ICard {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: Category;
+    price: number|null;
+    selected?: boolean; 
+}
+//Интерфейс данных пользователя
 export interface IUser {
-    adress: string,
-    email: string,
-    telephone: string,
+    adress: string;
+    email: string;
+    telephone: string;
     paymentWay: string
 }
 
-export interface ICardData {
-    cards: ICard[],
-    preview: string|null,
+//Интерфейс данных приложения
+export interface IAppData {
+    catalog: ICard[]; 
+    basket: ICard[]; 
+    order: IOrder | null; 
 }
 
-export interface IBasketData {
-    items: TBasket[],
+//интерфейс окна формы
+export interface IForm {
+    valid: boolean; 
+    errors: string[]; 
 }
 
-export type TCardInfo = Pick<ICard,'description'|'image'|'title'|'category'|'price'>
-export type TBasket = Pick<ICard,'title'|'price'>
-export type TPayment = Pick<IUser,'paymentWay'|'adress'>
-export type TUserData = Pick<IUser, 'telephone'|'email'>
+//интерфейс модального окна оформления заказа
+export interface IOrderForm {
+    address: string; 
+    payment: string; 
+}
 
-//TODO тип главной страницы
+//интерфейс 2 шага оформления заказа
+export interface IContactForm {
+    email: string; 
+    phone: string; 
+}
+
+export interface IOrder extends IOrderForm, IContactForm {
+    items: string[]; //список id товаров
+    total: number; //общая сумма заказа
+}
+
+//интерфейс корзины
+export interface IBasket {
+    items: HTMLElement[]; //список товаров
+    price: number; //итоговая стоимость заказа
+}
+
+//интерфейс успешное оформление заказа
+export interface IOrderSuccess {
+    id: string; 
+    count: number; //списано синапсов
+}
